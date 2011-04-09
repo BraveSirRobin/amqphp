b298b43cffa302afb1ce347ee527c94a73c05fae
Robin Harvey <harvey.robin@gmail.com>
2011-04-09 13:12:24 +0100

    Tidied up  the deom code  and put in  an explanation of the  TTL queue stuff

--- demos/demo-common.php
+++ demos/demo-common.php
@@ -29,18 +29,39 @@ $Q = 'most-basic';
 
 function initialiseDemo () {
     global $chan, $EX_TYPE, $EX_NAME, $Q;
-    $excDecl = $chan->exchange('declare', array('type' => $EX_TYPE,
-                                                'durable' => true,
-                                                'exchange' => $EX_NAME));
+    $excDecl = $chan->exchange('declare', array(
+                                   'type' => $EX_TYPE,
+                                   'durable' => true,
+                                   'exchange' => $EX_NAME));
     $eDeclResponse = $chan->invoke($excDecl); // Declare the queue
 
 
-    $qDecl = $chan->queue('declare', array('queue' => $Q)); // Declare the Queue
+    if (false) {
+        /**
+         * This code demonstrates  how to set a TTL  value on a queue.
+         * You have to  manually specify the table field  type using a
+         * TableField  object  because  RabbitMQ expects  the  integer
+         * x-message-ttl value to be  a long-long int (that's what the
+         * second parameter, 'l', in  the constructore means).  If you
+         * don't use a TableField,  Amqphp guesses the integer type by
+         * choosing the smallest possible storage type.
+         */
+        $args = new \amqp_091\wire\Table;
+        $args['x-message-ttl'] = new \amqp_091\wire\TableField(5000, 'l');
+
+        $qDecl = $chan->queue('declare', array(
+                                  'queue' => $Q,
+                                  'arguments' => $args)); // Declare the Queue
+    } else {
+        $qDecl = $chan->queue('declare', array('queue' => $Q));
+    }
+
     $chan->invoke($qDecl);
 
-    $qBind = $chan->queue('bind', array('queue' => $Q,
-                                        'routing-key' => '',
-                                        'exchange' => $EX_NAME));
+    $qBind = $chan->queue('bind', array(
+                              'queue' => $Q,
+                              'routing-key' => '',
+                              'exchange' => $EX_NAME));
     $chan->invoke($qBind);// Bind Q to EX
 
 }
